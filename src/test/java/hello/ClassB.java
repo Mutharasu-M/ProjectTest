c69c54496d2046690c5ecc589ea4a93621dcbc73
Mutharasu-M <mutharasu396@gmail.com>
2025-07-09 11:49:51 +0530

    Added methods and class B from Muthu

---
+++ src/test/java/hello/ClassB.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class ClassB {
+	
+	private void india() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void usa() {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
