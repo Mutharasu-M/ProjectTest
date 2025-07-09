634fef49f3d77a63f7b51e41ca44e9ec598caa1f
Mutharasu-M <mutharasu396@gmail.com>
2025-07-09 11:53:06 +0530

    Added few methods from Muthu

--- src/test/java/hello/HelloJava.java
+++ src/test/java/hello/HelloJava.java
@@ -8,4 +8,18 @@ public class HelloJava {
 		
 	}
 
+	private void method1() {
+		System.err.println("1");
+
+	}
+	private void method2() {
+		System.err.println("2");
+
+	}
+	
+	private void method3() {
+		System.err.println("3");
+
+	}
 }
+
